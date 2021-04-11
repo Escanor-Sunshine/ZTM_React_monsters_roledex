1299f49dfdd17670dff7e439f07a656bb3577874
Escanor Sunshine <manza.medina28@gmail.com>
2021-04-11 12:01:48 -0500

    Testing github pages

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 
         return (
             <div className='App'>
-                <h1>Monsters Roledex</h1>
+                <h1>Monsters Roledex Testing</h1>
                 <SearchBox
                     placeholder='Search people'
                     handleChange={(e) =>
